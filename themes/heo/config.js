3270938609dbbd90e88d492de9b546e875b05896
syx <982210873@qq.com>
2023-07-24 16:54:05 +0800

    heo主题定制

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2023-07-24', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到dubnation球迷社区', url: 'https://www.nba.com/warriors' },
+    { title: '访问nba官网以获取更多信息', url: 'https://www.nba.com' }
   ],
 
   // 英雄区(首页顶部大卡)
